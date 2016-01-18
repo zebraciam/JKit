58d1b418d4f6a055f13af9eb4dab9191b66a4828
陈杰 <mr_banma@126.com>
2016-01-18 13:34:27 +0800

    1.1.7  base64

--- JKit/Category/NSString+J.h
+++ JKit/Category/NSString+J.h
@@ -29,10 +29,32 @@
 #define JEncode(string) [string j_encode]
 #define JDecode(string) [string j_decode]
 
-
+#define JBase64( string )        [string j_base64]
+#define JText( string )        [string j_textFromBase64]
 
 
 @interface NSString (J)
+
+
+/******************************************************************************
+ 函数名称 : + (NSString *)base64StringFromText:(NSString *)text
+ 函数描述 : 将文本转换为base64格式字符串
+ 输入参数 : (NSString *)text    文本
+ 输出参数 : N/A
+ 返回参数 : (NSString *)    base64格式字符串
+ 备注信息 :
+ ******************************************************************************/
+- (NSString *)j_base64;
+
+/******************************************************************************
+ 函数名称 : + (NSString *)textFromBase64String:(NSString *)base64
+ 函数描述 : 将base64格式字符串转换为文本
+ 输入参数 : (NSString *)base64  base64格式字符串
+ 输出参数 : N/A
+ 返回参数 : (NSString *)    文本
+ 备注信息 :
+ ******************************************************************************/
+- (NSString *)j_textFromBase64;
 
 /**
  *  MD5
